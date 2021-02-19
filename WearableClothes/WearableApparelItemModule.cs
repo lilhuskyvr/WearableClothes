c5aaf6df1a984435a2d86a53fb3bf66c0bc5e2c4
Lil Husky VR <lilhuskyvr@gmail.com>
2021-02-19 22:16:31 +1100

    save

---
+++ WearableClothes/WearableApparelItemModule.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using ThunderRoad;
+using UnityEngine;
+
+namespace WearableClothes
+{
+    public class WearableApparelItemModule : ItemModule
+    {
+        public override void OnItemLoaded(Item item)
+        {
+            base.OnItemLoaded(item);
+            item.gameObject.AddComponent<WearableApparelItem>();
+        }
+    }
+
+
+    public class WearableApparelItem : MonoBehaviour
+    {
+        private Item _item;
+
+        private IEnumerator EquipWardrobe(Creature creature, Item item)
+        {
+            yield return new WaitForSeconds(1);
+
+            var itemData = Catalog.GetData<ItemData>(item.itemId);
+            item.holder.UnSnap(item);
+            item.Despawn(1);
+            creature.equipment.EquipWardrobe(
+                new ContainerData.Content(itemData)
+            );
+            yield return null;
+        }
+
+        private void Start()
+        {
+            _item = GetComponent<Item>();
+            _item.OnSnapEvent += holder => { StartCoroutine(EquipWardrobe(holder.creature, _item)); };
+        }
+    }
+}
