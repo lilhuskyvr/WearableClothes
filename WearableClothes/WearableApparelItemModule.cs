2eec44a6873f019f74d003d97f1c62ddd482d734
Lil Husky VR <lilhuskyvr@gmail.com>
2021-02-20 02:59:09 +1100

    save

--- WearableClothes/WearableApparelItemModule.cs
+++ WearableClothes/WearableApparelItemModule.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using ThunderRoad;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace WearableClothes
 {
@@ -8,7 +9,7 @@ namespace WearableClothes
     {
         public override void OnItemLoaded(Item item)
         {
-            base.OnItemLoaded(item);
+            base.OnItemLoaded(item); 
             item.gameObject.AddComponent<WearableApparelItem>();
         }
     }
@@ -20,13 +21,30 @@ namespace WearableClothes
 
         private IEnumerator EquipWardrobe(Creature creature, Item item)
         {
+            var itemData = Catalog.GetData<ItemData>(item.data.id);
+            item.holder.UnSnap(item, true);
+            item.Despawn();
+            
             yield return new WaitForSeconds(1);
 
-            var itemData = Catalog.GetData<ItemData>(item.data.id);
-            item.Despawn();
+            var content = new ContainerData.Content(itemData);
+            
+            if (creature.isPlayer)
+            {
+                var wardrobe = itemData.GetModule<ItemModuleWardrobe>().GetWardrobe(creature);
+                for (int index = 0; index < wardrobe.manikinWardrobeData.channels.Length; ++index)
+                {
+                    ContainerData.Content wornContent = creature.equipment.GetWornContent(wardrobe.manikinWardrobeData.channels[index], wardrobe.manikinWardrobeData.layers[index]);
+                    if (wornContent != null)
+                        GameManager.playerData.inventory.Remove(wornContent);
+                }
+                GameManager.playerData.inventory.Add(content);
+            }
+
             creature.equipment.EquipWardrobe(
-                new ContainerData.Content(itemData)
+                content
             );
+
             yield return null;
         }
 
